1478911e52e473509cb0bf534198e58330c26fa6
Pavlo-fo95 <Pavlo_fo95@student.itstep.org>
2024-06-11 01:01:05 +0300

    rotating

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isRotating, setIsRotating] = useState(false);
+
+  const toggleRotation = () => {
+    setIsRotating(!isRotating);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img
+  src={require('./profile.jpg')} 
+  className={isRotating ? "Profile-photo rotating" : "Profile-photo"}
+  alt="Алла Павлова" 
+  onClick={toggleRotation}
+  onMouseEnter={toggleRotation}
+  onMouseLeave={toggleRotation}
+/>
+        <h1>Surname: Pavlova</h1>
+        <h1>Name: Alla</h1>
+        <p>Age: 57</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Pavlo-fo95"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         My Github
         </a>
       </header>
     </div>
@@ -23,3 +36,5 @@ function App() {
 }
 
 export default App;
+
+
